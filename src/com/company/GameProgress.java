8a9380f6f5278b185b67b6f8ae2e22e57c0b7e14
Torsten Pause <torsten.pause@student.kyh.se>
2020-10-21 21:52:21 +0200

    placeYourShip() done

--- src/com/company/GameProgress.java
+++ src/com/company/GameProgress.java
@@ -1,18 +1,9 @@
 package com.company;
 
-import javafx.scene.media.AudioClip;
 
-import javax.sound.sampled.*;
 import javax.swing.*;
-import java.beans.Beans;
-import java.io.File;
-import java.io.File.*;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
+
 
 public class GameProgress {
     private final Game game;
@@ -61,100 +52,9 @@ public class GameProgress {
             userDialogueInitShip(currentShip);
         } else {
             placeYourShip(currentShip, actionCommand);
-            actionCase("");
+
         }
 
-    }
-
-        /*
-        if (actionCommand.equals("")) {
-            Ship currentShip = new Carrier(placedAtGridPosition[1]);
-            initCurrentShip(currentShip);
-        } else if (actionCommand.charAt(0) == 'l') {
-            Ship currentShip = new Carrier(placeHolder);
-            switch (placeHolder) {
-                case "Carrier":
-                    break;
-                case "Battleship":
-                    currentShip = new Battleship(placeHolder);
-                    break;
-                case "Cruiser":
-                    currentShip = new Cruiser(placeHolder);
-                    break;
-                case "Submarine":
-                    currentShip = new Submarine(placeHolder);
-                    break;
-                case "Destroyer":
-                    currentShip = new Destroyer(placeHolder);
-                    break;
-            }
-            leftGrid[Integer.parseInt(actionCommand.substring(5))][0] = currentShip.getShipNumber();
-            ArrayList<Integer> listCounter = new ArrayList<>();
-            for (int i = 0; i <= 100; i++) {
-                if (leftGrid[i][0] == currentShip.getShipNumber()) {
-                    listCounter.add(leftGrid[i][0]);
-                }
-            }
-            if (listCounter.size() == currentShip.getShipLength()) {
-                placeHolder = placedAtGridPosition[currentShip.getShipNumber() + 1];
-                currentShip = new Carrier(placeHolder);
-                switch (placeHolder) {
-                    case "Carrier":
-                        break;
-                    case "Battleship":
-                        currentShip = new Battleship(placeHolder);
-                        break;
-                    case "Cruiser":
-                        currentShip = new Cruiser(placeHolder);
-                        break;
-                    case "Submarine":
-                        currentShip = new Submarine(placeHolder);
-                        break;
-                    case "Destroyer":
-                        currentShip = new Destroyer(placeHolder);
-                        break;
-                    case "Game On":
-                        yourTurn = false;
-                        break;
-                }
-                initCurrentShip(currentShip);
-            }
-        }*/
-
-
-    void placeYourShip(Ship currentShip, String actionCommand) {
-        if(currentShip.getShipAlignment().equals("Vertical")) {
-            if(Integer.parseInt(actionCommand.substring(5)) <= 60 + ((5-currentShip.getShipLength())*10)) {
-                boolean empty = true;
-                for (int i = Integer.parseInt(actionCommand.substring(5)); i < (Integer.parseInt(actionCommand.substring(5)) + (currentShip.getShipLength() * 10)); i = i + 10) {
-                    if (game.getLeftGridValue(i, 0) != 0) {
-                        empty = false;
-                    }
-                }
-                if (empty) {
-                    for (int i = Integer.parseInt(actionCommand.substring(5)); i < (Integer.parseInt(actionCommand.substring(5)) + (currentShip.getShipLength() * 10)); i = i + 10) {
-                        game.setLeftGridValue(i, 0, currentShip.getShipNumber());
-                        gameBoard.leftB[i].setBackground(gameBoard.getShipFloating());
-                        gameBoard.leftB[i].setEnabled(false);
-                    }
-                    game.setPlaceHolder(game.getPlacedAtGridPosition(currentShip.getShipNumber() + 1));
-
-                }
-            } else {
-
-                try {
-                    File audioFile = new File("C:\\Users\\Maria Hermansson\\Dev\\ProjectFiles\\IdeaProjects\\Labs\\SinkingShipsGame\\gameData\\sound\\cantPlaceShipHere.wav");
-                    AudioClip sound = new AudioClip("" + audioFile.toURI());
-                    sound.play();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            //placeYourShip(currentShip, actionCommand);
-
-        } else {
-            //horizontal
-        }
     }
 
     void userDialogueInitShip(Ship currentShip) {
@@ -173,4 +73,45 @@ public class GameProgress {
             currentShip.setShipAlignment("Horizontal");
         }
     }
+
+    void placeYourShip(Ship currentShip, String actionCommand) {
+        if(currentShip.getShipAlignment().equals("Vertical")) {
+            if(Integer.parseInt(actionCommand.substring(5)) <= 60 + ((5-currentShip.getShipLength())*10)) {
+                boolean empty = true;
+                for (int i = Integer.parseInt(actionCommand.substring(5)); i < (Integer.parseInt(actionCommand.substring(5)) + (currentShip.getShipLength() * 10)); i = i + 10) {
+                    if (game.getLeftGridValue(i, 0) != 0) {
+                        empty = false;
+                    }
+                }
+                if (empty) {
+                    for (int i = Integer.parseInt(actionCommand.substring(5)); i < (Integer.parseInt(actionCommand.substring(5)) + (currentShip.getShipLength() * 10)); i = i + 10) {
+                        game.setLeftGridValue(i, 0, currentShip.getShipNumber());
+                        gameBoard.leftB[i].setBackground(gameBoard.getShipFloating());
+                        gameBoard.leftB[i].setEnabled(false);
+                    }
+                    game.setPlaceHolder(game.getPlacedAtGridPosition(currentShip.getShipNumber() + 1));
+                    actionCase("");
+                }
+            }
+        } else {
+            if(Integer.parseInt(actionCommand.substring(actionCommand.length()-1)) <= ((10-currentShip.getShipLength())+1)) {
+                boolean empty = true;
+                for (int i = Integer.parseInt(actionCommand.substring(5)); i < (Integer.parseInt(actionCommand.substring(5)) + (currentShip.getShipLength())); i++) {
+                    if (game.getLeftGridValue(i, 0) != 0) {
+                        empty = false;
+                    }
+                }
+                if (empty) {
+                    for (int i = Integer.parseInt(actionCommand.substring(5)); i < (Integer.parseInt(actionCommand.substring(5)) + (currentShip.getShipLength())); i++) {
+                        game.setLeftGridValue(i, 0, currentShip.getShipNumber());
+                        gameBoard.leftB[i].setBackground(gameBoard.getShipFloating());
+                        gameBoard.leftB[i].setEnabled(false);
+                    }
+                    game.setPlaceHolder(game.getPlacedAtGridPosition(currentShip.getShipNumber() + 1));
+                    actionCase("");
+                }
+            }
+        }
+    }
+
 }
