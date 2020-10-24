521e9b4b63635b70c12b82dd470b5ff73b13caa2
Torsten Pause <torsten.pause@student.kyh.se>
2020-10-24 19:00:41 +0200

    read/write + test

--- src/com/company/GameData.java
+++ src/com/company/GameData.java
@@ -1,16 +1,61 @@
 package com.company;
 
-import javax.swing.*;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 class GameData {
-    /*
+    private final String fileName = "./gameData/gameData.txt";
+    private Game game;
 
+    int[][] testArrayL = new int[5][2];
+    int[][] testArrayR = new int[5][2];
+
+
+
+
+    public GameData(){
+        this.game = game;
+/*
+        testArrayL[1][0] = 101;
+        testArrayL[2][0] = 201;
+        testArrayL[3][0] = 301;
+        testArrayL[4][0] = 401;
+
+        testArrayL[1][1] = 111;
+        testArrayL[2][1] = 211;
+        testArrayL[3][1] = 311;
+        testArrayL[4][1] = 411;
+
+        testArrayR[1][0] = 102;
+        testArrayR[2][0] = 202;
+        testArrayR[3][0] = 302;
+        testArrayR[4][0] = 402;
+
+        testArrayR[1][1] = 112;
+        testArrayR[2][1] = 212;
+        testArrayR[3][1] = 312;
+        testArrayR[4][1] = 412;
+
+ */
+
+
+        //printStringToFile(getStringFromArrayGridLeftAndArrayGridRight(testArrayL, testArrayR));
+        //getStringFromFileToArrayGridLeftAndArrayGridRight();
+    }
+
+    public static void main(String[] args) {
+        new GameData();
+
+    }
+
+
+/*
     public void saveGameData(Game game) {
         if (game.getPlaceHolder().equals("Carrier")) {
             JOptionPane noGame = new JOptionPane();
@@ -24,7 +69,7 @@ class GameData {
     }
         // ---------------------------------------
 
-        /*
+
 
         if(choseToSaveGame) {
             try{
@@ -36,7 +81,6 @@ class GameData {
                 e.printStackTrace();
             } finally { scan.close(); }
         }
-    }
 
 
     private void loadSavedGameData() {
@@ -64,7 +108,7 @@ class GameData {
     }
 
 
-         *//*
+
 
 
     public openFile(){
@@ -98,5 +142,56 @@ class GameData {
         String[] dateStrings = sc.nextLine().split(", ");
         return dateStrings;
     }
-    */
+        */
+    public String getStringFromArrayGridLeftAndArrayGridRight(int[][] arrayLeftGrid, int[][] arrayRightGrid){
+        StringBuilder contentToWriteToFile = new StringBuilder();
+
+        for(int i = 1; i <=arrayLeftGrid.length-1; i++){
+                contentToWriteToFile.append(arrayLeftGrid[i][0]).append(";");
+                contentToWriteToFile.append(arrayLeftGrid[i][1]).append(";");
+                contentToWriteToFile.append(arrayRightGrid[i][0]).append(";");
+                contentToWriteToFile.append(arrayRightGrid[i][1]).append(";");
+        }
+
+        return String.valueOf(contentToWriteToFile).substring(0, contentToWriteToFile.length()-1);
+    }
+
+    public void getStringFromFileToArrayGridLeftAndArrayGridRight(){
+        String stringReceivedFromFile = readStringFromFile();
+        String[] splitStringReceivedFromFile = stringReceivedFromFile.split(";");
+
+        int row = 1;
+        for(int i = 0; i <= (splitStringReceivedFromFile.length)-1; i+=4){
+            testArrayL[row][0] =  Integer.parseInt(splitStringReceivedFromFile[i]);
+            testArrayL[row][1] =  Integer.parseInt(splitStringReceivedFromFile[i+1]);
+            testArrayR[row][0] =  Integer.parseInt(splitStringReceivedFromFile[i+2]);
+            testArrayR[row][1] =  Integer.parseInt(splitStringReceivedFromFile[i+3]);
+            row++;
+        }
+    }
+
+    public void printStringToFile(String stringToPutToFile) {
+        try {
+            FileWriter inputToFile = new FileWriter(fileName);
+
+            inputToFile.write(stringToPutToFile);
+            inputToFile.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readStringFromFile(){
+        Scanner sc;
+        String stringInFile = "";
+
+        try {
+            sc = new Scanner(new File(fileName));
+            stringInFile = sc.nextLine();
+        } catch(Exception e){
+            System.err.println("Ouch! Something went terribly wrong...");
+        }
+        return stringInFile;
+    }
+
 }
---
+++ src/com/company/GameDataTest.java
@@ -0,0 +1,92 @@
+package com.company;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameDataTest {
+
+    private int[][] testArrayL = new int[3][2];
+    private int[][] testArrayR = new int[3][2];
+
+    private int[][] comparingTestArrayL = new int[3][2];
+    private int[][] comparingTestArrayR = new int[3][2];
+
+    @Test
+    @DisplayName("test to put arrays from both grids into string")
+    void testGettingStringFromTwo2DArrays(){
+
+        testArrayL[1][0] = 101;
+        testArrayL[2][0] = 201;
+
+        testArrayL[1][1] = 111;
+        testArrayL[2][1] = 211;
+
+        testArrayR[1][0] = 102;
+        testArrayR[2][0] = 202;
+
+        testArrayR[1][1] = 112;
+        testArrayR[2][1] = 212;
+
+        String expOutput = "101;111;102;112;201;211;202;212";
+        GameData gameData = new GameData();
+
+        assertEquals(expOutput, gameData.getStringFromArrayGridLeftAndArrayGridRight(testArrayL, testArrayR), "not the same output");
+    }
+
+    @Test
+    @DisplayName("test to write string to file")
+    void testToWriteStringToFile(){
+
+        String fileName = "./gameData/gameData.txt";
+        GameData gameData = new GameData();
+
+        gameData.printStringToFile("Test write and read to file.");
+
+        Scanner sc;
+        String stringInFile = "";
+
+        try {
+            sc = new Scanner(new File(fileName));
+            stringInFile = sc.nextLine();
+        } catch(Exception e){
+            System.err.println("Ouch! Something went terribly wrong...");
+        }
+
+        assertEquals("Test write and read to file.", stringInFile, "not the same string in file as expected");
+    }
+
+    @Test
+    @DisplayName("test to read string from file")
+    void testToReadStringFromFile(){
+
+        String fileName = "./gameData/gameData.txt";
+
+        String inputStringToFile = "Test reading from file.";
+
+        try {
+            FileWriter inputToFile = new FileWriter(fileName);
+
+            inputToFile.write(inputStringToFile);
+            inputToFile.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        GameData gameData = new GameData();
+
+        String actStringFromFile = gameData.readStringFromFile();
+
+        assertEquals(inputStringToFile, actStringFromFile, "not the same string in file as expected");
+
+    }
+
+
+
+
+}
